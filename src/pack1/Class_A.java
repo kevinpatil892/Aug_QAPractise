73c9eee31b9db4eec8cc1215176d3b412925d3bb
admin <admin@DESKTOP-OSG6H8E>
2025-09-07 10:20:59 +0530

    Commit code

---
+++ src/pack1/Class_A.java
@@ -0,0 +1,17 @@
+package pack1;
+
+import com.sun.tools.javac.launcher.Main;
+
+public class Class_A 
+{
+
+	public static void main(String[] args)
+	{
+	
+		System.out.println("Hello");
+	}
+	
+
+	
+	
+}
